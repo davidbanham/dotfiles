574c133d018b79f49dd4bdd7083a0fb7ac98fcaf
David Banham <david.banham@gmail.com>
2011-07-01 15:24:04 +1000

    Did something with submodules.

--- js/mail.google.com.js
+++ js/mail.google.com.js
@@ -1,24 +1,24 @@
 $(function () {
 
-var names = new Array();
-var favicons = new Array();
+    var names = new Array();
+    var favicons = new Array();
 
-names.push("Online Interactive Marketing Mail");
-favicons.push("http://oiminc.com.au/favicon.ico");
-names.push("Crnr Mail");
-favicons.push("http://crnr.com.au/crnrfavicon.png");
+    names.push("Online Interactive Marketing Mail");
+    favicons.push("http://oiminc.com.au/favicon.ico");
+    names.push("Crnr Mail");
+    favicons.push("http://crnr.com.au/crnrfavicon.png");
 
-var splitTitle = document.title.split(" - ");
+    var splitTitle = document.title.split(" - ");
 
-var link = document.createElement('link');
-link.setAttribute('rel', 'shortcut icon');
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'shortcut icon');
 
-for ( i in names ) {
+    for ( i in names ) {
 
     if ( splitTitle == names[i] ) {
         link.setAttribute('href', favicons[i]);
         $('head').append(link);
+        };
     };
-};
 
 });
