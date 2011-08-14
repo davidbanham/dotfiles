73727eda46f4589e1bb8a625362b6fbfcaf9b0ae
David Banham <david.banham@gmail.com>
2011-08-14 15:50:54 +1000

    Cleaned up indenting

--- js/mail.google.com.js
+++ js/mail.google.com.js
@@ -1,26 +1,26 @@
 $(function () {
 
-    var names = new Array();
-    var favicons = new Array();
+	var names = new Array();
+	var favicons = new Array();
 
-    names.push("Online Interactive Marketing Mail");
-    favicons.push("http://oiminc.com.au/favicon.ico");
-    names.push("Crnr Mail");
-    favicons.push("http://crnr.com.au/crnrfavicon.png");
-    names.push("Pinion Mail");
-    favicons.push("http://pinion.gg/pinionlogo.png");
+	names.push("Online Interactive Marketing Mail");
+	favicons.push("http://oiminc.com.au/favicon.ico");
+	names.push("Crnr Mail");
+	favicons.push("http://crnr.com.au/crnrfavicon.png");
+	names.push("Pinion Mail");
+	favicons.push("http://pinion.gg/pinionlogo.png");
 
-    var splitTitle = document.title.split(" - ");
+	var splitTitle = document.title.split(" - ");
 
-    var link = document.createElement('link');
-    link.setAttribute('rel', 'shortcut icon');
+	var link = document.createElement('link');
+	link.setAttribute('rel', 'shortcut icon');
 
-    for ( i in names ) {
+	for ( i in names ) {
 
-    if ( splitTitle == names[i] ) {
-        link.setAttribute('href', favicons[i]);
-        $('head').append(link);
-        };
-    };
+		if ( splitTitle == names[i] ) {
+			link.setAttribute('href', favicons[i]);
+			$('head').append(link);
+		};
+	};
 
 });
