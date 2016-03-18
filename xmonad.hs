349309519b68080479a755cbdb370e1bd9f1fe57
David Banham <david@banham.id.au>
2016-03-18 15:07:16 +1100

    Changes to xmonad conf

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,25 @@
--------------------------------------------------------------------------------
--- Main --
-main :: IO ()
-main = xmonad =<< statusBar cmd pp kb conf
-  where
-    uhook = withUrgencyHookC NoUrgencyHook urgentConfig
-    cmd = "xmobar -x0"
-    --cmd = "bash -c \"tee >(xmobar -x0) | xmobar -x1 | xmobar -x2\""
-    pp = customPP
-    kb = toggleStrutsKey
-    conf = uhook myConfig
+import XMonad
+import XMonad.Util.Run(spawnPipe)
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+import XMonad.Hooks.ManageDocks
+
+main = do
+
+  xmproc <- spawnPipe "/usr/bin/xmobar"
+  xmonad defaultConfig
+      { terminal    = "gnome-terminal"
+      , modMask     = mod4Mask
+      , borderWidth = 1
+      , focusFollowsMouse  = True
+      , workspaces = [
+        "1:code", "2:www", "3:comms", "4:email", "5:overflow", "6", "7", "8", "9"
+      ]
+      , manageHook = composeAll
+        [ resource =? "crx_bikioccmkafdpakkkcpdbppfkghcmihk" --> doShift "3:comms"
+        , resource =? "crx_nckgahadagoaajjgafhacjanaoiihapd" --> doShift "3:comms"
+        , resource =?"mail.google.com__mail_u_0" --> doShift "4:email"
+        , resource =? "mail.google.com__mail_u_1" --> doShift "4:email"
+        , manageDocks
+        ]
+      }
