e34f714dcc15e36a8fe6f6b19cb572dbdb139e64
David Banham <david@banham.id.au>
2016-05-23 22:38:40 +1000

    Move xmonad to new style

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Actions.Volume
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import XMonad.Hooks.ManageDocks
@@ -20,6 +21,7 @@ main = do
         [ resource =? "crx_bikioccmkafdpakkkcpdbppfkghcmihk" --> doShift "4:comms"
         , resource =? "crx_nckgahadagoaajjgafhacjanaoiihapd" --> doShift "4:comms"
         , resource =? "prismatik.slack.com" --> doShift "3:slack"
+        , resource =? "www.flowdock.com__app_prismatik" --> doShift "3:slack"
         , className =? "Slack" --> doShift "3:slack"
         , resource =? "mail.google.com__mail_u_0" --> doShift "5:email"
         , resource =? "mail.google.com__mail_u_1" --> doShift "5:email"
@@ -29,5 +31,10 @@ main = do
       , layoutHook = avoidStruts $ layoutHook defaultConfig
       }
       `additionalKeys`
-      [ ((mod4Mask, xK_w ), spawn "google-chrome")
+      [ ((mod4Mask, xK_w ), spawn "google-chrome"),
+        ((0, 0x1008ff13 ), raiseVolume 4 >> return ()),
+        ((0, 0x1008ff11 ), lowerVolume 4 >> return ()),
+        ((0, 0x1008ff12 ), spawn "amixer -D pulse set Master 1+ toggle"    >> return ()),
+        ((0, 0x1008ff03 ), spawn "xbacklight -20"),
+        ((0, 0x1008ff02 ), spawn "xbacklight +20")
       ]
