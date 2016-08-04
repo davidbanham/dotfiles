ab494f4612fbe11adc5284df0a02de971bbf41dc
David Banham <david@banham.id.au>
2016-08-04 19:36:39 +1000

    Switch up apps

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -15,17 +15,19 @@ main = do
       , borderWidth = 1
       , focusFollowsMouse  = True
       , workspaces = [
-        "1:code", "2:www", "3:slack", "4:comms", "5:email", "6:harvest", "7:enpass", "8", "9"
+        "1:code", "2:www", "3:slack", "4:comms", "5:email", "6:asana", "7:enpass", "8", "9"
       ]
       , manageHook = composeAll
         [ resource =? "crx_bikioccmkafdpakkkcpdbppfkghcmihk" --> doShift "4:comms"
         , resource =? "crx_nckgahadagoaajjgafhacjanaoiihapd" --> doShift "4:comms"
+        , resource =? "web.whatsapp.com" --> doShift "4:comms"
         , resource =? "prismatik.slack.com" --> doShift "3:slack"
         , resource =? "www.flowdock.com__app_prismatik" --> doShift "3:slack"
         , className =? "Slack" --> doShift "3:slack"
         , resource =? "mail.google.com__mail_u_0" --> doShift "5:email"
         , resource =? "mail.google.com__mail_u_1" --> doShift "5:email"
-        , resource =? "prismatik.harvestapp.com__time" --> doShift "6:harvest"
+        , resource =? "app.asana.com__0_161385269957318_list" --> doShift "6:asana"
+        , resource =? "calendar.google.com__calendar_b_1_render" --> doShift "6:asana"
         , manageDocks
         ]
       , layoutHook = avoidStruts $ layoutHook defaultConfig
