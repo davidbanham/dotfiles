faa6cc36b918d792774d19e67bfee08981b0eb3d
David Banham <david@banham.id.au>
2017-05-30 23:37:42 +1000

    Add glide to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,3 +37,5 @@ sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
+
+curl https://glide.sh/get | sh
