95b5526494e637814adbb7b2ff9251ad9444fbb2
David Banham <david@banham.id.au>
2017-06-21 11:21:01 +1000

    Moar binaries in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ wget -O - https://dl.sinew.in/keys/enpass-linux.key | apt-key add -
 sudo chown root:root /etc/apt/sources.list.d/enpass.list
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt update
-sudo apt install xmonad docker-ce docker-compose arandr enpass stow nodejs python-dev python-pip python3-dev python3-pip golang-go golang-go.tools ack-grep zsh build-essential cmake powertop laptop-mode-tools neovim lynx
+sudo apt install xmonad docker-ce docker-compose arandr enpass stow nodejs python-dev python-pip python3-dev python3-pip golang-go golang-go.tools ack-grep zsh build-essential cmake powertop laptop-mode-tools neovim lynx jq
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -25,8 +25,7 @@ source $HOME/.cargo/env
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
-npm install -g typescript
-npm install -g coffeescript
+npm install -g typescript coffeescript localtunnel yarn
 sudo pip3 install --upgrade neovim
 cd .config/nvim/plugged/YouCompleteMe/
 ./install.py --gocode-completer --clang-completer --tern-completer --racer-completer
@@ -44,3 +43,8 @@ curl -O https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
 sudo apt-get install -f
 run_keybase
+
+curl https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest > ~/.bin/ecs-cli && chmod +x ~/.bin/ecs-cli
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/develop/ecs-deploy > ~/.bin/ecs-deploy && chmod +x ~/.bin/ecs-deploy
+curl https://github.com/davidbanham/monty/raw/master/binaries/monty-linux-amd64 > ~/.bin/monty && chmod +x ~/.bin/monty
+cargo install watchexec
