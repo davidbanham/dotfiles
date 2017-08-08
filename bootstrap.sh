1d899972b7664462ec0e7657f99d814014177ac2
David Banham <david@banham.id.au>
2017-08-08 14:13:44 +1000

    Add livedown server to npm installation blurbs

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,7 +25,7 @@ source $HOME/.cargo/env
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
-npm install -g typescript coffeescript yarn
+npm install -g typescript coffeescript yarn livedown
 sudo pip3 install --upgrade neovim
 cd .config/nvim/plugged/YouCompleteMe/
 ./install.py --gocode-completer --clang-completer --tern-completer --racer-completer
