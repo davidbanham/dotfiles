4f38fd3b5f4c9284754230155d12c8e6f9b19c66
David Banham <david@banham.id.au>
2017-09-01 10:27:04 +1000

    Add urlscan to packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ wget -O - https://dl.sinew.in/keys/enpass-linux.key | apt-key add -
 sudo chown root:root /etc/apt/sources.list.d/enpass.list
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt update
-sudo apt install xmonad docker-ce docker-compose arandr enpass stow nodejs python-dev python-pip python3-dev python3-pip golang-go golang-go.tools ack-grep zsh build-essential cmake powertop laptop-mode-tools neovim lynx jq htop autoconf ffmpeg imagemagick abook fluxgui
+sudo apt install xmonad docker-ce docker-compose arandr enpass stow nodejs python-dev python-pip python3-dev python3-pip golang-go golang-go.tools ack-grep zsh build-essential cmake powertop laptop-mode-tools neovim lynx jq htop autoconf ffmpeg imagemagick abook fluxgui urlscan
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
