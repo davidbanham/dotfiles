fe8a12e2c1b72333554e122c44166e896328f42e
David Banham <david@banham.id.au>
2018-02-20 20:23:16 +1100

    Fix up bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,28 @@
+#!/bin/bash
+set -euxo pipefail
+
 sudo apt update
 sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
-sudo add-apt-repository ppa:longsleep/golang-backports
 sudo add-apt-repository ppa:nathan-renniewaldock/flux
 sudo add-apt-repository ppa:gezakovacs/ppa
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 
 wget https://pkg.tarsnap.com/tarsnap-deb-packaging-key.asc
 sudo apt-key add tarsnap-deb-packaging-key.asc
 echo "deb http://pkg.tarsnap.com/deb/$(lsb_release -s -c) ./" | sudo tee -a /etc/apt/sources.list.d/tarsnap.list
 
+wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
+
 sudo apt update
-sudo apt install xmonad docker-ce docker-compose arandr stow nodejs python-dev python-pip python3-dev python3-pip golang-go golang-go.tools ack-grep zsh build-essential cmake powertop laptop-mode-tools neovim lynx jq htop autoconf ffmpeg imagemagick abook fluxgui urlscan pass direnv task libfreetype6-dev libfontconfig1-dev xclip unetbootin weechat tarsnap
+sudo apt install xmonad docker-ce docker-compose arandr stow nodejs python-dev python-pip python3-dev python3-pip golang-go golang-go.tools zsh build-essential cmake powertop laptop-mode-tools neovim lynx jq htop autoconf ffmpeg imagemagick abook fluxgui urlscan pass direnv task libfreetype6-dev libfontconfig1-dev xclip unetbootin weechat tarsnap tmux cargo
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-$ curl -sSL https://github.com/passff/passff/releases/download/1.0.0/install_host_app.sh | bash -s -- firefox
+curl -sSL https://github.com/passff/passff/releases/download/1.0.0/install_host_app.sh | bash -s -- firefox
 
 cd ~/repos/dotfiles
 ./install.sh
@@ -29,7 +34,7 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
-mkdir ~/.npm-global
+mkdir -p ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 npm install -g typescript coffeescript yarn livedown coffee-script nesh
@@ -37,8 +42,9 @@ sudo pip3 install --upgrade neovim
 sudo pip install neovim
 sudo pip install --upgrade git+git://github.com/tbabej/tasklib@develop
 pip install --user --upgrade sqlparse
-cd .config/nvim/plugged/YouCompleteMe/
-./install.py --gocode-completer --clang-completer --tern-completer --racer-completer
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # For wee-slack
 sudo pip install websocket-client
@@ -49,8 +55,6 @@ sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
-
-curl https://glide.sh/get | sh
 
 curl -O https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
@@ -83,4 +87,4 @@ sudo cp target/release/alacritty /usr/local/bin
 cp Alacritty.desktop ~/.local/share/applications
 cd ~
 
-go install github.com/davidbanham/termcolour
+go get github.com/davidbanham/termcolour
