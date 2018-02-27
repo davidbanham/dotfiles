c69b2a16403b3f9caf4f4398cdafbc22ddb74a41
David Banham <david@banham.id.au>
2018-02-27 22:51:49 +1100

    Add TLP for power management

--- bootstrap_arch.sh
+++ bootstrap_arch.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 pacman -S xfce4 xf86-video-intel xorg-server base-devel openssh
 
-pacman -S neovim python-neovim redshift docker nodejs tarsnap go xmonad stow python python2 zsh jq htop ffmpeg imagemagick abook pass task xclip weechat tmux mutt inkscape firefox npm python-pip adobe-source-code-pro-fonts ripgrep fzf hugo python-setuptools python2-setuptools rustup scour python-websocket-client ttf-freefont ttf-liberation ttf-droid docker-compose
+pacman -S neovim python-neovim redshift docker nodejs tarsnap go xmonad stow python python2 zsh jq htop ffmpeg imagemagick abook pass task xclip weechat tmux mutt inkscape firefox npm python-pip adobe-source-code-pro-fonts ripgrep fzf hugo python-setuptools python2-setuptools rustup scour python-websocket-client ttf-freefont ttf-liberation ttf-droid docker-compose tlp
 
 cd aur
 for i in $( ls ); do
@@ -17,8 +17,8 @@ for i in $( ls ); do
 done
 cd ..
 
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
+usermod -aG docker $USER
+systemctl enable docker lightdm.service tlp.service tlp-sleep.service
 
 mkdir -p ~/.npm-global
 npm config set prefix '~/.npm-global'
