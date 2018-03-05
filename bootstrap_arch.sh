39f8c714b2b4799313b072f8bb69fb9c3e6bc27b
David Banham <david@banham.id.au>
2018-03-05 20:57:25 +1100

    Add services

--- bootstrap_arch.sh
+++ bootstrap_arch.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 pacman -S xfce4 xf86-video-intel xorg-server base-devel openssh
 
-pacman -S neovim python-neovim redshift docker nodejs tarsnap go xmonad stow python python2 zsh jq htop ffmpeg imagemagick abook pass task xclip weechat tmux mutt inkscape firefox npm python-pip adobe-source-code-pro-fonts ripgrep fzf hugo python-setuptools python2-setuptools rustup scour python-websocket-client python2-websocket-client ttf-freefont ttf-liberation ttf-droid docker-compose tlp xdg-utils lynx postgresql bc arduino pulseaudio pulseaudio-alsa pulseaudio-bluetooth xfce4-pulseaudio-plugin
+pacman -S neovim python-neovim redshift docker nodejs tarsnap go xmonad stow python python2 zsh jq htop ffmpeg imagemagick abook pass task xclip weechat tmux mutt inkscape firefox npm python-pip adobe-source-code-pro-fonts ripgrep fzf hugo python-setuptools python2-setuptools rustup scour python-websocket-client python2-websocket-client ttf-freefont ttf-liberation ttf-droid docker-compose tlp xdg-utils lynx postgresql bc arduino pulseaudio pulseaudio-alsa pulseaudio-bluetooth xfce4-pulseaudio-plugin pavucontrol
 
 cd aur
 for i in $( ls ); do
@@ -18,7 +18,7 @@ done
 cd ..
 
 usermod -aG docker $USER
-systemctl enable docker lightdm.service tlp.service tlp-sleep.service
+systemctl enable docker lightdm.service tlp.service tlp-sleep.service pulseaudio.service netctl-auto@wlp2s0.service
 
 mkdir -p ~/.npm-global
 npm config set prefix '~/.npm-global'
